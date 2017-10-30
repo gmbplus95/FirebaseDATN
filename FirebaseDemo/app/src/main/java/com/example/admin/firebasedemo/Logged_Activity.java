8dbb51e661b5f3e4a28e1b6d6fa90df48275c1b5
Anh Bac <gmbplus95@gmail.com>
2017-10-31 06:40:57 +0700

    add MS cognitive services

--- FirebaseDemo/app/src/main/java/com/example/admin/firebasedemo/Logged_Activity.java
+++ FirebaseDemo/app/src/main/java/com/example/admin/firebasedemo/Logged_Activity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -14,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.webkit.MimeTypeMap;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,83 +25,162 @@ import com.facebook.login.LoginManager;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.google.gson.Gson;
+import com.microsoft.projectoxford.vision.VisionServiceClient;
+import com.microsoft.projectoxford.vision.VisionServiceRestClient;
+import com.microsoft.projectoxford.vision.contract.AnalysisResult;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+
 
 public class Logged_Activity extends AppCompatActivity {
+    public VisionServiceClient vsc =new VisionServiceRestClient("a20888090e864633b0abe9441bd70e63","https://westcentralus.api.cognitive.microsoft.com/vision/v1.0");
     private TextView email_logged;
     private StorageReference mStorageRef;
     private DatabaseReference mDatabaseRef;
     private ImageView imageView;
     private EditText txtImageName;
     private Uri imgUri;
-    public static final String FB_STORAGE_PATH="image/";
-    public static final String FB_DATABASE_PATH="image";
-    public static final int REQUEST_CODE=1234;
+    public static final String FB_STORAGE_PATH = "image/";
+    public static final String FB_DATABASE_PATH = "image";
+    public static final int REQUEST_CODE = 1234;
+    private Button btn_check;
+    private Button btn_upload;
+    private Button btn_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_);
-        email_logged= (TextView) findViewById(R.id.Email_logged);
+        email_logged = (TextView) findViewById(R.id.Email_logged);
         email_logged.setText(getIntent().getExtras().getString("Email"));
         mStorageRef = FirebaseStorage.getInstance().getReference();
-        mDatabaseRef= FirebaseDatabase.getInstance().getReference(FB_DATABASE_PATH);
-        imageView= (ImageView) findViewById(R.id.imageView);
-        txtImageName= (EditText) findViewById(R.id.txtImageName);
-        imageView.setImageResource(0);
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference(FB_DATABASE_PATH);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        txtImageName = (EditText) findViewById(R.id.txtImageName);
+        btn_check =findViewById(R.id.btn_check);
+        btn_upload=findViewById(R.id.button3);
+        btn_view=findViewById(R.id.button2);
+        btn_check.setEnabled(false);
     }
 
     public void browser_onclick(View view) {
-        Intent intent=new Intent();
+        Intent intent = new Intent();
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent,"Select Image"),REQUEST_CODE);
+        startActivityForResult(Intent.createChooser(intent, "Select Image"), REQUEST_CODE);
+        btn_check.setEnabled(true);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==REQUEST_CODE && resultCode==RESULT_OK && data!=null && data.getData()!=null){
-            imgUri =data.getData();
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imgUri = data.getData();
             try {
-                Bitmap bm= MediaStore.Images.Media.getBitmap(getContentResolver(),imgUri);
+                btn_upload.setEnabled(true);
+                Bitmap bm = MediaStore.Images.Media.getBitmap(getContentResolver(), imgUri);
                 imageView.setImageBitmap(bm);
+                //convert image to stream
+                ByteArrayOutputStream outputStream= new ByteArrayOutputStream();
+                bm.compress(Bitmap.CompressFormat.JPEG,80,outputStream);
+                final ByteArrayInputStream inputStream=new ByteArrayInputStream(outputStream.toByteArray());
+                btn_check.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        final AsyncTask<InputStream,String,String> visionTask=new AsyncTask<InputStream, String, String>() {
+                            ProgressDialog mdialog=new ProgressDialog(Logged_Activity.this);
+                            @Override
+                            protected String doInBackground(InputStream... params) {
+                                try {
+                                    publishProgress("Checking...");
+                                    String[] features={"Adult"};
+                                    String[] details={};
+                                    AnalysisResult result=vsc.analyzeImage(params[0],features,details);
+                                    String strResult=new Gson().toJson(result);
+                                    return strResult;
+                                } catch (Exception e) {
+                                      return null;
+                                }
+                            }
+
+                            @Override
+                            protected void onPreExecute() {
+                                mdialog.show();
+                            }
+
+                            @Override
+                            protected void onPostExecute(String s) {
+                                mdialog.dismiss();
+                                AnalysisResult result=new Gson().fromJson(s,AnalysisResult.class);
+                                TextView txtdetail=findViewById(R.id.check_detail);
+                                if(result.adult.isAdultContent==true) {
+                                    txtdetail.setText("Adult Content");
+                                    Toast.makeText(Logged_Activity.this,"Ảnh không được chấp nhận!",Toast.LENGTH_SHORT).show();
+                                    btn_check.setEnabled(false);
+                                    btn_upload.setEnabled(false);
+                                }
+                                else if(result.adult.isRacyContent==true) {
+                                    txtdetail.setText("Racy Content");
+                                    Toast.makeText(Logged_Activity.this,"Ảnh không được chấp nhận!",Toast.LENGTH_SHORT).show();
+                                    btn_check.setEnabled(false);
+                                    btn_upload.setEnabled(false);
+                                } else {
+                                    txtdetail.setText("Accepted !");
+                                    Toast.makeText(Logged_Activity.this,"Ảnh được chấp nhận!",Toast.LENGTH_SHORT).show();
+                                    btn_upload.setEnabled(true);
+                                }
+
+                            }
+
+                            @Override
+                            protected void onProgressUpdate(String... values) {
+                                mdialog.setMessage(values[0]);
+                            }
+                        };
+                        visionTask.execute(inputStream);
+                    }
+
+                });
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+
         }
     }
-    public String getImagetxt(Uri uri){
-        ContentResolver contentResolver =getContentResolver();
-        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
-        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
 
+    public String getImagetxt(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
     }
+
     @SuppressWarnings("VisibleForTests")
     public void Upload_onclick(View view) {
         if (TextUtils.isEmpty(txtImageName.getText().toString())) {
             txtImageName.setError("Ban Chua Nhap Ten Anh");
             return;
-        } else
-            {
+        } else {
             if (imgUri != null) {
                 final ProgressDialog dialog = new ProgressDialog(this);
                 dialog.setTitle("Uploading...");
                 dialog.show();
                 StorageReference ref = mStorageRef.child(FB_STORAGE_PATH + txtImageName.getText().toString().trim() + "." + getImagetxt(imgUri));
-                        ref.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                            @Override
-                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                ref.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                         dialog.dismiss();
                         Toast.makeText(getApplicationContext(), "Upload Successfully", Toast.LENGTH_SHORT).show();
                         ImageUpload imageUpload = new ImageUpload(txtImageName.getText().toString(), taskSnapshot.getDownloadUrl().toString());
@@ -129,8 +210,8 @@ public class Logged_Activity extends AppCompatActivity {
             }
         }
     }
-    public void onBackPressed()
-    {
+
+    public void onBackPressed() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Bạn muốn thoát khỏi chương trình?");
         builder.setCancelable(true);
@@ -156,7 +237,17 @@ public class Logged_Activity extends AppCompatActivity {
     }
 
     public void showimg_onclick(View view) {
-        Intent i=new Intent(Logged_Activity.this,ImageListActivity.class);
+        Intent i = new Intent(Logged_Activity.this, ImageListActivity.class);
         startActivity(i);
     }
+
+    @Override
+    protected void onResume() {
+        btn_upload.setEnabled(false);
+        super.onResume();
+    }
 }
+
+
+
+
